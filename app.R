36bcbc3b6899592d5579ccd029d6b7c363e01be0
Christof Neumann <christofneumann1@gmail.com>
2023-01-18 23:52:05 +0100

    move functionality out to functions

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(rhandsontable)
 
 source("helpers/make_empty_objects.R")
 source("helpers/html_styles.R")
+source("helpers/review_tables.R")
 
 source("helpers/empty_foc_table.R")
 source("helpers/reload_sessions.R")
@@ -254,64 +255,12 @@ server <- function(input, output, session) {
 
 
   # reviewing of existing data ---------------------------
-  output$rev_focal_table <- renderRHandsontable({
-    sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
-    fp <- file.path(paths_day$dirpath, paste0(sess, "_foctab.csv"))
-    if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
-      # outtab <- read.csv(paste0("www/", sess, ".csv"))
-      outtab <- read.csv(fp)
-      print(fp)
-      outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
-      # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
-      # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
-      outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
-      outtab
-    }
-  })
-  output$rev_nn <- renderRHandsontable({
-    sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
-    # p <- paste0("www/", sess, "-nn.csv")
-    fp <- file.path(paths_day$dirpath, paste0(sess, "_nn.csv"))
-    if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
-      if (!isTRUE(readLines(fp) == "")) {
-        outtab <- read.csv(fp)
-        colnames(outtab) <- c("id", paste0("scan", seq_len(ncol(outtab) - 1)))
-        print(head(outtab))
-        outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
-        # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
-        # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
-        outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
-        outtab
-      }
-    }
-  })
-  output$rev_groom <- renderRHandsontable({
-    sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
-    fp <- file.path(paths_day$dirpath, paste0(sess, "_groom.csv"))
-    if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
-      outtab <- read.csv(fp)[-1, ]
-      # print(head(outtab))
-      outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
-      # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
-      # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
-      outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
-      outtab
-    }
-  })
-  output$rev_aggression <- renderRHandsontable({
-    sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
-    fp <- file.path(paths_day$dirpath, paste0(sess, "_aggr.csv"))
-    if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
-      outtab <- read.csv(fp)
-      outtab <- outtab[-nrow(outtab), ]
-      # print(head(outtab))
-      outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
-      # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
-      # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
-      outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
-      outtab
-    }
-  })
+  output$rev_focal_table <- renderRHandsontable(review_table_foctab(input = input, paths_day = paths_day, metadata = metadata))
+  output$rev_nn <- renderRHandsontable(review_table_nn(input = input, paths_day = paths_day, metadata = metadata))
+  output$rev_groom <- renderRHandsontable(review_table_groom(input = input, paths_day = paths_day, metadata = metadata))
+  output$rev_aggression <- renderRHandsontable(review_table_aggr(input = input, paths_day = paths_day, metadata = metadata))
+
+
 
 
 
---
+++ helpers/review_tables.R
@@ -0,0 +1,70 @@
+review_table_foctab <- function(input, paths_day, metadata) {
+  sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
+  fp <- file.path(paths_day$dirpath, paste0(sess, "_foctab.csv"))
+  if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
+    # outtab <- read.csv(paste0("www/", sess, ".csv"))
+    outtab <- read.csv(fp)
+    # print(fp)
+    outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
+    # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
+    # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
+    outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
+    return(outtab)
+  }
+  NULL
+}
+
+
+review_table_nn <- function(input, paths_day, metadata) {
+  sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
+  fp <- file.path(paths_day$dirpath, paste0(sess, "_nn.csv"))
+  if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
+    if (!isTRUE(readLines(fp) == "")) {
+      outtab <- read.csv(fp)
+      colnames(outtab) <- c("id", paste0("scan", seq_len(ncol(outtab) - 1)))
+      # print(head(outtab))
+      outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
+      # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
+      # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
+      outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
+      return(outtab)
+    }
+  }
+  NULL
+}
+
+review_table_groom <- function(input, paths_day, metadata) {
+  sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
+  fp <- file.path(paths_day$dirpath, paste0(sess, "_groom.csv"))
+  if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
+    outtab <- read.csv(fp)[-1, ]
+    # print(head(outtab))
+    outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
+    # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
+    # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
+    outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
+    return(outtab)
+  }
+  NULL
+}
+
+review_table_aggr <- function(input, paths_day, metadata) {
+  sess <- gsub(".*\\((.*)\\).*", "\\1", input$session_for_review)
+  fp <- file.path(paths_day$dirpath, paste0(sess, "_aggr.csv"))
+  if (metadata$focal_sessions_so_far > 0 & file.exists(fp)) {
+    outtab <- read.csv(fp)
+    outtab <- outtab[-nrow(outtab), ]
+    # print(head(outtab))
+    outtab <- rhandsontable(outtab, rowHeaders = NULL, height = 500)
+    # outtab <- hot_col(outtab, "scratches", readOnly = TRUE)
+    # hot_table(outtab, highlightCol = TRUE, highlightRow = TRUE)
+    outtab <- hot_context_menu(outtab, allowRowEdit = FALSE, allowColEdit = FALSE)
+    return(outtab)
+  }
+  NULL
+}
+
+
+
+
+
